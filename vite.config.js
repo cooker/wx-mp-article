2284a2ef02948cd6bff292b58047a9989e640946
Grant <fishkq@aliyun.com>
2026-01-22 17:29:15 +0800

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,26 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { fileURLToPath } from 'url'
+import { dirname, resolve } from 'path'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 export default defineConfig({
   plugins: [vue()],
+  base: './',
   server: {
     port: 3000,
-    open: true
+    open: false
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html')
+      }
+    }
   }
 })
